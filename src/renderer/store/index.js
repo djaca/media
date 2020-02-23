734e12a16b2f25eb329f6a8e1434f52ebbbd62f5
Aleksandar Savanovic <djaca85@gmail.com>
2020-02-23 08:45:30 +0100

    Store vuex states to external file

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,18 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import VuexPersistence from 'vuex-persist'
+import createPersistedState from 'vuex-persistedstate'
+import ElectronStore from 'electron-store'
 
 import modules from './modules'
 
-const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
+const store = new ElectronStore()
+
+const opt = {
+  storage: {
+    getItem: key => store.get(key),
+    setItem: (key, value) => store.set(key, value),
+    removeItem: key => store.delete(key)
+  },
   reducer: (state) => ({Favorites: state.Favorites, Settings: state.Settings})
-})
+}
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules,
-  plugins: [vuexLocal.plugin],
+  plugins: [createPersistedState(opt)],
   strict: process.env.NODE_ENV !== 'production'
 })
